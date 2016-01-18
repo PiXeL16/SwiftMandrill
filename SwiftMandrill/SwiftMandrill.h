931946e1c7b936e8fb3d35caa18a86e0b57aede9
Christopher Jimenez <cjimenez16@gmail.com>
2016-01-18 15:16:28 -0600

    Initial Commit

---
+++ SwiftMandrill/SwiftMandrill.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMandrill.h
+//  SwiftMandrill
+//
+//  Created by Christopher Jimenez on 1/18/16.
+//  Copyright © 2016 greenpixels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMandrill.
+FOUNDATION_EXPORT double SwiftMandrillVersionNumber;
+
+//! Project version string for SwiftMandrill.
+FOUNDATION_EXPORT const unsigned char SwiftMandrillVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMandrill/PublicHeader.h>
+
+
